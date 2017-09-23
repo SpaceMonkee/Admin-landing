f09359810b3298737ca9ff57cad7b1e283964e0e
SpaceMonkee <joshthebro4@gmail.com>
2017-09-23 11:10:29 +0200

    Add files via upload

---
+++ java/vuemain.js
@@ -0,0 +1,22 @@
+//page header
+var app = new Vue({
+    el: '#sender',
+    data: {
+        user1: 'Jane Doe',
+    }
+});
+var app = new Vue({
+    el: '#sender1',
+    data: {
+        user2: 'John Doe',
+    }
+});
+
+//icon  update times
+
+var app = new Vue({
+  el: '#app-2',
+  data: {
+    messages: ' Last Updated at ' + new Date().toLocaleString()
+  }
+});
